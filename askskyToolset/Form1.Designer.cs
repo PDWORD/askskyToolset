53dcdaf36c6f5b01a30eeb654bd685673415eac3
PDWORD <asksky0521@gmail.com>
2020-09-17 21:29:09 +0800

    新增圖片轉文字功能

--- askskyToolset/Form1.Designer.cs
+++ askskyToolset/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(159, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "文字轉圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(79, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 134);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(405, 304);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "字串";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(302, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 38);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "保存圖片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(17, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 38);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "設定字型";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(439, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "圖片轉文字";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- askskyToolset/Form1.cs
+++ askskyToolset/Form1.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +19,62 @@ namespace askskyToolset
         {
             InitializeComponent();
         }
+
+        #region 文字轉圖片功能
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 獲得要轉成圖片的字串/字體/大小/顏色
+            string text = textBox1.Text;
+            Font font = fontDialog1.Font;
+            SolidBrush drawBrush = new SolidBrush(fontDialog1.Color);
+
+            // 獲得圖像所需所需大小
+            Bitmap bitmap = new Bitmap(1, 1);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            SizeF sizeF = graphics.MeasureString(text, font);
+
+            //重設Bitmap大小
+            int width = (int)(sizeF.Width + 1);
+            int height = (int)(sizeF.Height + 1);
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            bitmap.Dispose();
+            bitmap = new Bitmap(width, height);
+            graphics = Graphics.FromImage(bitmap);
+
+            // 繪製文字
+            graphics.DrawString(text, font, drawBrush, rect);
+            pictureBox1.Image = bitmap;
+        }
+        #endregion
+
+        #region 另存圖片
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 設定可處存的圖片類型
+            var imageFormat = new ImageFormat[] { ImageFormat.Png, ImageFormat.Jpeg, ImageFormat.Bmp };
+
+            // 初始另存新檔
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = textBox1.Text;
+            saveFileDialog.Filter = "PNG files (*.png)|*.png|JPEG files (*.jpeg)|*.jpeg|BMP files (*.jpeg)|*.bmp";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                bitmap.Save(saveFileDialog.FileName, imageFormat[saveFileDialog.FilterIndex - 1]);
+
+            }
+        }
+        #endregion
+
+        #region 獲得字型
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;
+            fontDialog1.ShowDialog();
+        } 
+        #endregion
     }
 }
